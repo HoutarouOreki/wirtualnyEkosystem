2f5f59d3b23cdff90db56d01a4970e255ff5827d
Hōtarō Oreki <TheZjarek@gmail.com>
2020-03-27 02:03:15 +0100

    Zmień folder dokumentacji z /html/ na /docs/

---
+++ docs/html/menudata.js
@@ -0,0 +1,60 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"Strona główna",url:"index.html"},
+{text:"Klasy",url:"annotated.html",children:[
+{text:"Lista klas",url:"annotated.html"},
+{text:"Indeks klas",url:"classes.html"},
+{text:"Hierarchia klas",url:"hierarchy.html"},
+{text:"Składowe klas",url:"functions.html",children:[
+{text:"Wszystko",url:"functions.html",children:[
+{text:"c",url:"functions.html#index_c"},
+{text:"d",url:"functions.html#index_d"},
+{text:"g",url:"functions.html#index_g"},
+{text:"i",url:"functions.html#index_i"},
+{text:"k",url:"functions.html#index_k"},
+{text:"m",url:"functions.html#index_m"},
+{text:"n",url:"functions.html#index_n"},
+{text:"o",url:"functions.html#index_o"},
+{text:"p",url:"functions.html#index_p"},
+{text:"s",url:"functions.html#index_s"},
+{text:"u",url:"functions.html#index_u"},
+{text:"w",url:"functions.html#index_w"},
+{text:"z",url:"functions.html#index_z"},
+{text:"~",url:"functions.html#index__7E"}]},
+{text:"Funkcje",url:"functions_func.html",children:[
+{text:"c",url:"functions_func.html#index_c"},
+{text:"d",url:"functions_func.html#index_d"},
+{text:"g",url:"functions_func.html#index_g"},
+{text:"i",url:"functions_func.html#index_i"},
+{text:"m",url:"functions_func.html#index_m"},
+{text:"o",url:"functions_func.html#index_o"},
+{text:"p",url:"functions_func.html#index_p"},
+{text:"s",url:"functions_func.html#index_s"},
+{text:"u",url:"functions_func.html#index_u"},
+{text:"w",url:"functions_func.html#index_w"},
+{text:"z",url:"functions_func.html#index_z"},
+{text:"~",url:"functions_func.html#index__7E"}]},
+{text:"Zmienne",url:"functions_vars.html"}]}]},
+{text:"Pliki",url:"files.html",children:[
+{text:"Lista plików",url:"files.html"}]}]}
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "bcdgikmnopsuwz~",
+  1: "bgosu",
+  2: "cdgimopsuwz~",
+  3: "cikmnpswz"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Wszystko",
+  1: "Klasy",
+  2: "Funkcje",
+  3: "Zmienne"
+};
+
